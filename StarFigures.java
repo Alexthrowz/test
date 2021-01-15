848a6777800144f05055e299e10f051519a799a8
Alexthrowz <76978816+Alexthrowz@users.noreply.github.com>
2021-01-15 15:31:47 -0800

    StarFigures
    Matched class name to file name

--- StarFigures.java
+++ StarFigures.java
@@ -1,4 +1,4 @@
-class Main {
+class StarFigures {
   public static void main(String[] args) {
     System.out.println("*****\n*****\n * *\n  *\n * *\n \n*****\n*****\n * *\n  *\n * *\n*****\n*****\n \n  *\n  *\n  *\n*****\n*****\n * *\n  *\n * *");
   }
